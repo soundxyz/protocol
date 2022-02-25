761d7ea86bdd6da06d9c7312ab90c84eff8d34fb
Matt Masurka <m.masurka@gmail.com>
2022-02-25 13:09:23 -0800

    hardhat tasks and config updates
    - Updates hardhat tasks
    - Fixes compiler config in hardhat.config
    - Adds hardhat-contract-sizer

--- protocol/tasks/verify.ts
+++ protocol/tasks/verify.ts
@@ -1,4 +1,7 @@
+import { constants } from '@soundxyz/common';
 import { task } from 'hardhat/config';
+
+const { baseURIs } = constants;
 
 task('verify-contract', 'Verify a contract')
   .addParam('name', 'The name of the contract')
@@ -6,13 +9,20 @@ task('verify-contract', 'Verify a contract')
   .setAction(async (args, hardhat) => {
     const { ethers, run, deployments } = hardhat;
     const { name, address } = args;
+    const version = name.split('ArtistV')[1];
+
+    if (!version) {
+      throw new Error(`Invalid contract name: ${name}. Must have a version number`);
+    }
+
+    const baseURI = baseURIs[hardhat.network.name];
 
     const argsForArtistInit = [
       '0xB0A36b3CeDf210f37a5E7BC28d4b8E91D4E3C412', // deployer address
       '0',
-      `Sound.xyz ArtistV2.sol`,
-      `SOUND V2`,
-      'https://sound.xyz/api/metadata/',
+      `Sound.xyz ArtistV${version}.sol`,
+      `SOUND V${version}`,
+      baseURI,
     ];
 
     const artistCreator = await ethers.getContract('ArtistCreator');
@@ -25,6 +35,6 @@ task('verify-contract', 'Verify a contract')
 
     await run('verify:verify', {
       address: address,
-      constructorArguments: beaconConstructorArgs,
+      constructorArguments: name == 'UpgradeableBeacon' ? beaconConstructorArgs : [],
     });
   });
