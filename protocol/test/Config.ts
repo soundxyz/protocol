1873c1e5e481761c07e7f00a90908e62abfa3526
Matt Masurka <m.masurka@gmail.com>
2022-03-23 09:44:23 -0700

    Adds solidity-coverage

---
+++ protocol/test/Config.ts
@@ -0,0 +1,47 @@
+// import '@nomiclabs/hardhat-ethers';
+
+// import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signers';
+// import { helpers as commonHelpers } from '@soundxyz/common';
+// import { expect } from 'chai';
+// import { BigNumber, Contract } from 'ethers';
+// import { parseEther } from 'ethers/lib/utils';
+// import { ethers, waffle } from 'hardhat';
+
+// import {
+//   BASE_URI,
+//   currentSeconds,
+//   deployArtistImplementation,
+//   deployArtistProxy,
+//   EMPTY_SIGNATURE,
+//   EXAMPLE_ARTIST_ID,
+//   EXAMPLE_ARTIST_NAME,
+//   EXAMPLE_ARTIST_SYMBOL,
+//   getRandomBN,
+//   getRandomInt,
+//   getTokenId,
+//   INVALID_PRIVATE_KEY,
+//   MAX_UINT32,
+//   NULL_ADDRESS,
+//   NULL_TICKET_NUM,
+// } from './helpers';
+
+// type CustomMintArgs = {
+//   quantity?: BigNumber;
+//   price?: BigNumber;
+//   startTime?: BigNumber;
+//   endTime?: BigNumber;
+//   editionCount?: number;
+//   royaltyBPS?: BigNumber;
+//   fundingRecipient?: SignerWithAddress;
+//   permissionedQuantity?: BigNumber;
+//   skipCreateEditions?: boolean;
+//   signer?: SignerWithAddress;
+// };
+
+// const { getPresaleSignature } = commonHelpers;
+
+// const { provider } = waffle;
+
+// const chainId = 1337;
+
+class Config {}
